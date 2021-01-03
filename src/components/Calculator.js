5162fb79e64226e9f5052e0165b0e7e5f6f0d856
FarahNajib <farahengnajib@gmail.com>
2021-01-03 13:13:25 +0800

    TBD

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,3 +1,21 @@
+import React from 'react';
+import Formula from './Formula'
+import Button from './Button'
+import Output from './Output'
+const projectName = 'javascript-calculator';
+
+const isOperator = /[x/+‑]/,
+  endsWithOperator = /[x+‑/]$/,
+  endsWithNegativeSign = /\d[x/+‑]{1}‑$/,
+  clearStyle = { background: '#ac3939' },
+  operatorStyle = { background: '#666666' },
+  equalsStyle = {
+    background: '#004466',
+    position: 'absolute',
+    height: 130,
+    bottom: 5
+  };
+
 class Calculator extends React.Component {
     constructor(props) {
       super(props);
--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Calculator from './components/Calculator'
 
 
-const projectName = 'javascript-calculator';
-
-const isOperator = /[x/+‑]/,
-  endsWithOperator = /[x+‑/]$/,
-  endsWithNegativeSign = /\d[x/+‑]{1}‑$/,
-  clearStyle = { background: '#ac3939' },
-  operatorStyle = { background: '#666666' },
-  equalsStyle = {
-    background: '#004466',
-    position: 'absolute',
-    height: 130,
-    bottom: 5
-  };
 
 
 
